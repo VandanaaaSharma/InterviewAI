33e444cd66246bcd4fce539d217c2009c3f1c6e2
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2026-03-09 22:02:52 +0530

    done

--- resume_parser.py
+++ resume_parser.py
@@ -1,1 +1,12 @@
+import PyPDF2
 
+def extract_text(uploaded_file):
+
+    reader = PyPDF2.PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return text
